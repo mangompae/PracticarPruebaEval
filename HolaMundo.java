67e4051da9492aa76c95272c21846980dc2f67a3
mangompae <mgomezpaez280705@gmail.com>
2024-10-29 18:55:43 +0100

    V1Colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,8 @@
 public class HolaMundo {
 
     public static void main(String[] args) {
+        String rojo= "\033[32m";
+        String azul= "\033[34m";
         System.out.println("Hola mundo");
         System.out.println("Ejemplos git");
     }
